258cb499a40ca2780aab79c9fbabf250791717e5
Felvesthe <23108389+Felvesthe@users.noreply.github.com>
2026-03-24 21:43:36 +0100

    feat: enable strict mode for Eloquent models
    Signed-off-by: Felvesthe <23108389+Felvesthe@users.noreply.github.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Telescope\TelescopeServiceProvider;
 
@@ -23,5 +24,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void {}
+    public function boot(): void
+    {
+        Model::shouldBeStrict();
+    }
 }
