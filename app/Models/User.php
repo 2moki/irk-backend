b64dd94000599ff23cb95f000be35ff4cde94665
Felvesthe <23108389+Felvesthe@users.noreply.github.com>
2026-03-24 18:45:55 +0100

    feat(api): install laravel api support with sanctum
    Signed-off-by: Felvesthe <23108389+Felvesthe@users.noreply.github.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 #[Fillable([
     'first_name',
@@ -30,6 +31,7 @@ use Illuminate\Notifications\Notifiable;
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
+    use HasApiTokens;
     /** @use HasFactory<UserFactory> */
     use HasFactory;
     use Notifiable;
