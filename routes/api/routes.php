7ec8960b036055dac00d7a65d2e2d5d8d4aa8454
Felvesthe <23108389+Felvesthe@users.noreply.github.com>
2026-03-29 18:17:59 +0200

    feat(api): add sanctum authentication to v1 routes
    Signed-off-by: Felvesthe <23108389+Felvesthe@users.noreply.github.com>

--- routes/api/routes.php
+++ routes/api/routes.php
@@ -7,10 +7,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['throttle:api'])->prefix('v1')->as('v1:')->group(function (): void {});
+Route::middleware(['auth:sanctum', 'throttle:api'])->prefix('v1')->as('v1:')->group(function (): void {
+    Route::get('/user', fn(Request $request) => $request->user())->name('user');
+});
 
 if (App::isLocal()) {
     Route::post('dev-token', AuthTokenController::class)->name('dev-token');
 }
-
-Route::get('/user', fn(Request $request) => $request->user())->middleware('auth:sanctum');
