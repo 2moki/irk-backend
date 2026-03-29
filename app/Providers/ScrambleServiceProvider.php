94d66703b7059aabd89b01c2f54101531f5fab93
Felvesthe <23108389+Felvesthe@users.noreply.github.com>
2026-03-29 18:13:59 +0200

    feat(scramble): require Bearer token for authentication
    Signed-off-by: Felvesthe <23108389+Felvesthe@users.noreply.github.com>

---
+++ app/Providers/ScrambleServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
+use Illuminate\Support\ServiceProvider;
+
+final class ScrambleServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        Scramble::configure()
+            ->withDocumentTransformers(function (OpenApi $openApi): void {
+                $openApi->secure(
+                    SecurityScheme::http('bearer'),
+                );
+            });
+    }
+}
