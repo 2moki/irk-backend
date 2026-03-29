3d9a1102b1e0ad70a8c5ff08e544213cab01aad3
Felvesthe <23108389+Felvesthe@users.noreply.github.com>
2026-03-29 22:00:59 +0200

    feat(scramble): exclude api/auth route from adding to the documentation
    Signed-off-by: Felvesthe <23108389+Felvesthe@users.noreply.github.com>

--- app/Providers/ScrambleServiceProvider.php
+++ app/Providers/ScrambleServiceProvider.php
@@ -7,7 +7,9 @@ namespace App\Providers;
 use Dedoc\Scramble\Scramble;
 use Dedoc\Scramble\Support\Generator\OpenApi;
 use Dedoc\Scramble\Support\Generator\SecurityScheme;
+use Illuminate\Routing\Route;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 final class ScrambleServiceProvider extends ServiceProvider
 {
@@ -18,6 +20,7 @@ final class ScrambleServiceProvider extends ServiceProvider
                 $openApi->secure(
                     SecurityScheme::http('bearer'),
                 );
-            });
+            })
+            ->routes(fn(Route $route): bool => Str::startsWith($route->uri, 'api/') && ! Str::startsWith($route->uri, 'api/auth'));
     }
 }
