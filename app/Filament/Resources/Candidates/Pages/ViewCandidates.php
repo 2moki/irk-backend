eaa8f0d9ed34ac4101b5b5ab82e6302cadad916c
Amigorn <braum123@o2.pl>
2026-06-01 16:31:36 +0200

    fix and added field address in candidates details

--- app/Filament/Resources/Candidates/Pages/ViewCandidates.php
+++ app/Filament/Resources/Candidates/Pages/ViewCandidates.php
@@ -4,8 +4,21 @@ namespace App\Filament\Resources\Candidates\Pages;
 
 use App\Filament\Resources\Candidates\CandidatesResource;
 use Filament\Resources\Pages\ViewRecord;
+use Filament\Actions\EditAction;
 
 class ViewCandidates extends ViewRecord
 {
     protected static string $resource = CandidatesResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            EditAction::make(),
+        ];
+    }
+
+    public function getTitle(): string
+    {
+        return 'Podgląd kandydata';
+    }
 }
